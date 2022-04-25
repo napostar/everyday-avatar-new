1dfb716830a7e1bf8ef85993a1154464168991d9
nathan.powelson <nathan.powelson@sisu.us>
2022-04-25 12:31:03 -0500

    updated  avatar contract, implemented minting and updating tokens, with dynamic tokenURI generation.

--- contracts/EverydayAvatar.sol
+++ contracts/EverydayAvatar.sol
@@ -22,6 +22,7 @@ import "./ERC3664/extensions/ERC3664Updatable.sol";
  * The Everyday Avatar project was designed with the following requirements/features:
  *   -Unlimited mints (technically limited to the range of uint256...)
  *   -Customizable On-Chain Attributes with the power of EIP-3664
+ *     -Using the value of ERC3664 attributes to represent the assetId for the nft collection
  *   -Utilizes Chainlink Any-API to update the token's IPFS image when it's visual attributes are changed
  *   - 
  */
@@ -29,53 +30,116 @@ contract EverydayAvatar is ERC721, ERC721URIStorage, ERC3664Updatable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-	
-	//TODO need to finalize the list of attribute categories
-	uint constant BODY = 1;
+    
+    //mint fee can be updated by contract owner
+    uint256 public mintFee;
+    
+    //List of attribute/nft part categories (Using Invisible Friends demo content)   
+    uint constant BACKGROUND = 1;
     uint constant HEAD = 2;
-    uint constant EYES = 3;
-    uint constant HAIR = 4;
+    uint constant FACE = 3;
+    uint constant CLOTHES = 4;
 
     constructor() ERC721("Everyday Avatar", "EA") ERC3664("") {
-        //TODO mint/create ERC3664 attribute categories
-		ERC3664._mint(BODY, "body", "Body", "");
-		ERC3664._mint(HEAD, "head", "Head", "");
-		ERC3664._mint(EYES, "eyes", "Eyes", "");
-		ERC3664._mint(HAIR, "hair", "Hair", "");
+      //Create ERC3664 attribute categories (attributeID, symbol text, json text, uri)
+      ERC3664._mint(BACKGROUND, "bg", "Background", "");
+      ERC3664._mint(HEAD, "head", "Head", "");
+      ERC3664._mint(FACE, "face", "Face", "");
+      ERC3664._mint(CLOTHES, "clothes", "Clothes", "");
+      
+      //initial mint fee will be 10 MATIC (~$15 usd) (deploying on Polygon)
+      mintFee = 10 ether;
     }
 	
-	//mint an avatar with the provided attributes, which will come from the dApp UI (ie user selection)
-    function mintAvatar(address to, uint256[] memory attr, uint256[] memory attrValue) public payable {
-		//TODO add asserts
+    //mint an avatar with the provided attributes, which will come from the dApp UI (ie user selection)
+    function mintAvatar(address to, uint256[] memory attrId, uint256[] memory attrValue) public payable {
+		//TODO add needed require checks
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         
-        for(uint i=0; i < attr.length ; i++){
-			ERC3664.attach(tokenId, attr[i], attrValue[i]);
+        for(uint i=0; i < attrId.length ; i++){
+          ERC3664.attach(tokenId, attrId[i], attrValue[i]);
         }
     }
 	
-	//TODO function to build json metadata
-	//TODO implement payment withdraw function
-	//TODO implement function to update token image URI from oracle (use '_setTokenURI(tokenId, uri)' )
-	//TODO implement chainlink Any-API when attributes change
-	//TODO implement function to update token attributes (scoped to only the token owner)
-	
+    //TODO implement function to update token image URI from oracle (use '_setTokenURI(tokenId, uri)' )
+    //TODO implement chainlink Any-API when attributes change
+    //TODO implement function to lookup string name of attribute based on id
+    
+    //update token attributes (scoped to only the token owner)
+    function updateAvatar(uint256 tokenId, uint256[] memory attrId, uint256[] memory attrValue) public {
+      //TODO validate user is owner using _msgSender(); (support meta transactions)
+      //TODO add needed require checks
+      for(uint i=0 ; i < attrId.length ; i++) {
+        //detatch and re-attach new ERC3664 attribute.  Would be nice if ERC3664 had a setValue instead of just increase/decrease
+        remove(tokenId, attrId[i]);
+        attach(tokenId, attrId[i], attrValue[i]);
+      }
+    }   
+    //TODO need a way to convert attribute value to a string.
+    //helper function to build json metadata
+    function getAttributes(uint256 tokenId) public view returns (string memory) {
+      uint256[] memory attributes = attributesOf(tokenId);
+      uint256[] memory values = this.balanceOfBatch(tokenId, attributes);
+      bytes memory output;
+      
+      for(uint i=0 ; i < attributes.length ; i++) {
+      
+        output = abi.encodePacked(output, '{"trait_type":"', name(attributes[i]), '","value":"', uintToBytes(values[i]), '"}');
+      }
+      
+      return string(output);
+    }
+    
+    //build the json uri for the specified tokenId
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory)
+    {
+        string memory attributes = getAttributes(tokenId);
+        string memory json = Base64.encode(bytes(string(abi.encodePacked(
+          '{"name": "Everyday Avatar',  //would be cool if name could come from the metacore identity contract
+          ' #', 
+          uintToBytes(tokenId), 
+          '", "description":"Everyday Avatars are a collection of profile picture NFTs that are completely customizable. You can freely modify and update your Avatar using the dApp. Attributes are stored on-chain and this amazing flexibility is powered by EIP-3664.", "image":"', 
+          //TODO ADD IMAGE IPFS
+          '","attributes":['
+          , attributes, 
+          ']}'
+        ))));
 
+        return string(abi.encodePacked('data:application/json;base64,', json));
+    }
+    
+    //update the minting fee, need to keep the price in the $10-20 usd range (TODO use a price feeds oracle for this with a keeper to update automatically?)
+    function updateMintFee(uint256 newFee) public onlyOwner {
+      mintFee = newFee;
+    }
+    
+    //withdraw function for funds (TODO look into PaymentSplitter contract)
+    function ownerWithdraw(address payable to) public onlyOwner {
+      to.transfer(address(this).balance);
+    }
+    
+    //convert number to string (technically bytes...)
+    function uintToBytes(uint v) internal pure returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
+    
+    
     // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC3664) returns (bool)
