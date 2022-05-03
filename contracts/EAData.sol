61862c2c263224c0a8654b2b42a76b421411278d
nathan.powelson <nathan.powelson@sisu.us>
2022-05-02 22:41:04 -0500

    updated data contract to be more clear

--- contracts/EAData.sol
+++ contracts/EAData.sol
@@ -9,28 +9,51 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 */
 contract AvatarNameData is Ownable {
 
-    //mapping attribute value to name string
+    //mapping component to name string
     mapping(uint => string) public _names;
 
-    function componentName(uint attrValue) public view returns(string memory) {
-        return _names[attrValue];
+    //mapping componentIds to attributes
+    mapping(uint => uint) public _compAttrs;
+
+    //get the name of the component
+    function componentName(uint componentId) public view returns(string memory) {
+        return _names[componentId];
     }
 
-    function componentNames(uint[] memory attrValues) public view returns(string[] memory) {
-        string[] memory output = new string[](attrValues.length);
-        for(uint i=0; i < attrValues.length ; i++) {
-            output[i] = _names[attrValues[i]];
+    //batch lookup of component names
+    function componentNames(uint[] memory compValues) public view returns(string[] memory) {
+        string[] memory output = new string[](compValues.length);
+        for(uint i=0; i < compValues.length ; i++) {
+            output[i] = _names[compValues[i]];
         }
         return output;
     }
 
-    function addComponent(uint value, string calldata name) public onlyOwner{
-        _names[value] = name;
+    //add a new component to data
+    function addComponent(uint componentId, uint attrId, string calldata name) public onlyOwner{
+        _compAttrs[componentId] = attrId;
+        _names[componentId] = name;
     }
 
-    function addManyComponents(uint[] memory values, string[] memory names) public onlyOwner {
-        for(uint i=0; i < values.length ; i++) {
-            _names[values[i]] = names[i];
+    //batch add components
+    function addManyComponents(uint[] memory componentId, uint[] memory attrIds, string[] memory names) public onlyOwner {
+        for(uint i=0; i < componentId.length ; i++) {
+            _names[componentId[i]] = names[i];
+            _compAttrs[componentId[i]] = attrIds[i];
         }
+    }
+
+    //lookup attributeId for the given component
+    function componentAttribute(uint componentId) public view returns(uint) {
+        return _compAttrs[componentId];
+    }
+
+    //lookup many attributeIds for the given components
+    function batchComponentAttributes(uint[] memory componentIds) public view returns (uint[] memory) {
+        uint[] memory output = new uint[](componentIds.length);
+        for(uint i=0 ; i < componentIds.length ; i++) {
+            output[i]=_compAttrs[componentIds[i]];
+        }
+        return output;
     }
 }
